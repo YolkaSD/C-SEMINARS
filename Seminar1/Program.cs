e90155b6dae9be20bc0a339ccecff29a94dd4f94
Slava Rybkin <yolka.sd@gmail.com>
2022-12-18 13:21:03 +0700

    Added work at the first seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Семинар 1.
+
+// Задача 1.
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num2 * num2;
+
+// if(num1 == quad2)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else 
+// {
+//     Console.WriteLine("No");
+// }
+
+//Задача 2.
+
+// Console.Write("Input a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int current = -number;
+
+// while(current <= number)
+// {
+//     Console.Write($"{current} ");
+//     current++;
+// }
