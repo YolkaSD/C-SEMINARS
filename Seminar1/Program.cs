d3594b6808fb4e87402916a37b9a153eb968fcb4
Slava Rybkin <yolka.sd@gmail.com>
2022-12-18 14:08:47 +0700

    Added 4 work at the first seminar

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -80,3 +80,14 @@
 // {
 //     Console.Write($"{num} Number not a even");
 // }
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 2; i <= num; i ++){
+    if(i % 2 == 0) 
+    {
+        Console.Write($"{i} ");
+    }
+}
