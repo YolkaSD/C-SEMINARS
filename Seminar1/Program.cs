9a47b679ad7251ab72def132820f49b430191aa5
Slava Rybkin <yolka.sd@gmail.com>
2022-12-18 13:57:06 +0700

    Added 2 work at the first seminar

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -31,20 +31,40 @@
 // }
 
 //Home Work Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.Write("Input a first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
-{
-    Console.WriteLine("The first number is greater: ");
-}
-else if(num1 == num2)
-{
-    Console.WriteLine("The numbers are equal");
-}
-else
-{
-    Console.WriteLine("The second number is more: ");
-}
+// if(num1 > num2)
+// {
+//     Console.WriteLine("The first number is greater: ");
+// }
+// else if(num1 == num2)
+// {
+//     Console.WriteLine("The numbers are equal");
+// }
+// else
+// {
+//     Console.WriteLine("The second number is more: ");
+// }
+
+//Home Work Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// int[] arrayNum = new int[3];
+// Console.Write("Input a first number: ");
+// arrayNum[0] = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// arrayNum[1] = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a third number: ");
+// arrayNum[2] = Convert.ToInt32(Console.ReadLine());
+
+// int maxNumber = arrayNum[0];
+// for(int i = 1; i < arrayNum.Length; i ++)
+// {
+//     if(maxNumber < arrayNum[i]) 
+//     {
+//         maxNumber = arrayNum[i];
+//     }
+// }
+
+// Console.WriteLine($"The largest number is: {maxNumber}");
